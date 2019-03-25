d118cafe324841b5943b1ae16725ed9868bed268
Darren Shankland <ds@4pq.co.uk>
2019-03-25 15:35:53 +0000

    Part A done

---
+++ codeclan_student.rb
@@ -0,0 +1,31 @@
+class Student
+
+  def initialize(name, cohort)
+    @name = name
+    @cohort = cohort
+  end
+
+  def get_student_name()
+    return @name
+  end
+
+  def get_student_cohort()
+    return @cohort
+  end
+
+  def set_student_name(name)
+    @name = name
+  end
+
+  def set_student_cohort(cohort)
+    @cohort = cohort
+  end
+
+  def speaks()
+    return "I can talk!"
+  end
+
+  def say_favourite_language(language)
+    return "I love #{language}!"
+  end
+end
