8f0e55471f97d329e72f9b22b312de166af79627
Darren Shankland <ds@4pq.co.uk>
2019-03-25 16:43:21 +0000

    Added add_points method

--- specs/test_sports_team.rb
+++ specs/test_sports_team.rb
@@ -25,4 +25,43 @@ class TeamTest < MiniTest::Test
     assert_equal('Lenny', team.coach)
   end
 
+  def test_add_player
+    team = Team.new('Rangers', ['Novo', 'Prso'], 'StevieG')
+    assert_equal(['Novo', 'Prso', 'Goram'], team.add_player('Goram'))
+  end
+
+  def test_contains_player_true
+    team = Team.new('Rangers', ['Novo', 'Prso'], 'StevieG')
+    assert_equal(true,team.contains_player('Prso'))
+  end
+
+  def test_contains_player_false
+    team = Team.new('Rangers', ['Novo', 'Prso'], 'StevieG')
+    assert_equal(false,team.contains_player('Messi'))
+  end
+
+  def test_contains_new_player
+    team = Team.new('Rangers', ['Novo', 'Prso'], 'StevieG')
+    team.add_player('Goram')
+    assert_equal(['Novo', 'Prso', 'Goram'], team.players)
+  end
+
+  def test_add_points_win
+    team = Team.new('Rangers', ['Novo', 'Prso'], 'StevieG')
+    team.add_points('win')
+    assert_equal(3, team.points)
+  end
+
+  def test_add_points_lose
+    team = Team.new('Rangers', ['Novo', 'Prso'], 'StevieG')
+    team.add_points('lose')
+    assert_equal(0, team.points)
+  end
+
+  def test_add_points_draw
+    team = Team.new('Rangers', ['Novo', 'Prso'], 'StevieG')
+    team.add_points('draw')
+    assert_equal(1, team.points)
+  end
+
 end
--- sports_team.rb
+++ sports_team.rb
@@ -1,12 +1,26 @@
 class Team
 
-  attr_reader :teamname, :players
+  attr_reader :teamname, :players, :points
   attr_accessor :coach
 
   def initialize(teamname, players, coach)
     @teamname = teamname
     @players = players
     @coach = coach
+    @points = 0
+  end
+
+  def add_player(player)
+    @players.push(player)
+  end
+
+  def contains_player(player)
+    return @players.include?(player)
+  end
+
+  def add_points(win_lose_draw)
+    @points += 3 if win_lose_draw == 'win'
+    @points += 1 if win_lose_draw == 'draw'
   end
 
 end
